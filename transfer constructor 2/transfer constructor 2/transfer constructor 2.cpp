c8c8885809a5802c1a6a3b686d2743a112897bf4
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-09-10 21:30:39 +0300

    Add files via upload

---
+++ transfer constructor 2/transfer constructor 2/transfer constructor 2.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <utility> // для std::move
+using namespace std;
+
+class Array
+{
+private:
+    int* data;
+    size_t size;
+
+public:
+    Array() : data(nullptr), size(0) {}
+
+    Array(size_t size) : size(size)
+    {
+        data = new int[size];
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = 0;
+        }
+    }
+
+    Array(const Array& other)
+    {
+        size = other.size;
+        data = new int[size];
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = other.data[i];
+        }
+    }
+
+    Array& operator=(const Array& other)
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+        delete[] data;
+        size = other.size;
+        data = new int[size];
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = other.data[i];
+        }
+        return *this;
+    }
+
+    Array(Array&& other) noexcept : data(other.data), size(other.size)
+    {
+        other.data = nullptr;
+        other.size = 0;
+    }
+
+    Array& operator=(Array&& other) noexcept
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+        delete[] data;
+        data = other.data;
+        size = other.size;
+
+        other.data = nullptr;
+        other.size = 0;
+
+        return *this;
+    }
+
+    ~Array()
+    {
+        delete[] data;
+    }
+
+    void print() const
+    {
+        if (data)
+        {
+            for (size_t i = 0; i < size; ++i)
+            {
+                cout << data[i] << " ";
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "Пустой массив" << endl;
+        }
+    }
+
+    void set(size_t index, int value)
+    {
+        if (index < size)
+        {
+            data[index] = value;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Array arr1(5);
+    arr1.set(0, 10);
+    arr1.set(1, 20);
+    arr1.set(2, 30);
+    arr1.set(3, 40);
+    arr1.set(4, 50);
+
+    cout << "массив 1: ";
+    arr1.print();
+
+    Array arr2 = move(arr1);
+
+    cout << "массив 1 после перемещения: ";
+    arr1.print();
+
+    cout << "массив 2: ";
+    arr2.print();
+
+    return 0;
+}
---
+++ transfer constructor/transfer constructor/transfer constructor.cpp
@@ -0,0 +1,92 @@
+﻿#define _CRT_SECURE_NO_WARNINGS //чтобы не выдавало ошибку
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class String
+{
+private:
+    char* data;
+    size_t size;
+
+public:
+    String() : data(nullptr), size(0) {}
+
+    String(const char* str)
+    {
+        size = strlen(str);
+        data = new char[size + 1];
+        strcpy(data, str);
+    }
+
+    String(const String& other)
+    {
+        size = other.size;
+        data = new char[size + 1];
+        strcpy(data, other.data);
+    }
+
+    String& operator=(const String& other)
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+        delete[] data;
+        size = other.size;
+        data = new char[size + 1];
+        strcpy(data, other.data);
+        return *this;
+    }
+
+    String(String&& other) noexcept : data(other.data), size(other.size)
+    {
+        other.data = nullptr; 
+        other.size = 0;
+    }
+
+    String& operator=(String&& other) noexcept
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+        delete[] data; 
+        data = other.data;
+        size = other.size;
+
+        other.data = nullptr;
+        other.size = 0;
+
+        return *this;
+    }
+
+    ~String()
+    {
+        delete[] data;
+    }
+
+    // Вывод строки
+    void print() const
+    {
+        if (data)
+        {
+            cout << data << endl;
+        }
+        else {
+            cout << "Пустая строка" << endl;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    String str1("Hello");
+    String str2 = move(str1); 
+
+    str1.print();
+    str2.print();
+
+    return 0;
+}
